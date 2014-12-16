17afaacf0de661301eea821f4b6f8a933284e93a
Russ Taylor <russt@palantir.com>
2014-12-15 22:08:54 -0800

    * Updating Forge * Removing old Marble blocks * Starting adding iron stairs

---
+++ src/main/java/net/minecraft/lodecraftia/block/vanilla/IronStairs.java
@@ -0,0 +1,22 @@
+package net.minecraft.lodecraftia.block.vanilla;
+
+import net.minecraft.block.Block;
+import net.minecraft.lodecraftia.block.ModBlockStairs;
+
+/**
+ * Custom class for Iron Stairs.
+ *
+ * Created by russt on 12/2/14.
+ */
+public class IronStairs extends ModBlockStairs {
+
+    public static String BLOCK_NAME = "iron-stairs";
+
+    public IronStairs(Block parent) {
+        super(parent, parent.getDefaultState(), BLOCK_NAME);
+    }
+
+    public ModBlockStairs register() {
+        return register(BLOCK_NAME);
+    }
+}
