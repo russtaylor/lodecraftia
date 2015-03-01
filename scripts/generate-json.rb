3550032f8bb13ee003dd665808e40afd30c1666c
Russ Taylor <russ@russt.me>
2015-03-01 10:34:37 -0500

    Making good progress. Yay!

--- scripts/generate-json.rb
+++ scripts/generate-json.rb
@@ -8,6 +8,7 @@ require 'fileutils'
 
 type_options = [:fence, :slab, :stairs, :wall, :block]
 model_directories = ['models/block', 'blockstates', 'models/item']
+non_custom_options = ['block_name', 'mod_name']
 
 # Handle the command line options.
 options = {}
@@ -19,7 +20,7 @@ optionParser = OptionParser.new do |opts|
   end
 
   opts.on('-n', '--name NAME', 'The name of the block you\'re generating.') do |name|
-    options[:name] = name;
+    options[:block_name] = name;
   end
 
   opts.on('-x', '--texture NAME', 'The name of the texture used for this block') do |texture|
@@ -33,7 +34,7 @@ end
 
 begin
   optionParser.parse!
-  mandatory = [:type, :name]
+  mandatory = [:type, :block_name]
   missing = mandatory.select{ |param| options[param].nil? }
   if not missing.empty?
     puts "Missing required options: #{missing.join(', ')}"
@@ -68,9 +69,10 @@ end
 def parse_options(options)
   if not options[:custom].nil?
     split = options[:custom].split(',')
-    split_options = Array.new
+    split_options = Hash.new
     split.each do |option|
-      split_options << option.split('=')
+      current_option = option.split('=')
+      split_options[current_option[0]] = current_option[1]
     end
   end
   options[:custom] = split_options
@@ -104,9 +106,11 @@ def find_templates(options, source, directories)
 end
 
 def copy_templates(options, templates, destination)
+  puts destination
   new_sources = Array.new
   templates.each do |current_file|
-    new_filename = current_file.gsub(/template/, options[:name])
+    new_filename = current_file.gsub(/template/, options[:block_name])
+    new_filename = new_filename.gsub(/json-source\//, '')
     FileUtils.cp(current_file, "#{destination}/#{new_filename}")
     new_sources << new_filename
   end
@@ -124,16 +128,41 @@ def set_texture_names(options, source_text)
   end
 end
 
-def edit_content(options, directory, file_list)
+def edit_content(passed_options, file_options, directory, file_list)
   file_list.each do |file_name|
     full_path = directory + '/' + file_name
     file_text = File.read(full_path)
-    file_text.gsub!(/\{mod_name\}/, options[:mod] + ":")
-    file_text = set_texture_names(options, file_text)
+    file_text = replace_options(passed_options, file_options, file_text)
     File.open(full_path, 'w') do |file|
       file.puts(file_text)
     end
   end
+end
+
+def validate_options(passed_option_list, file_option_list, non_custom_options)
+  missing_options = Array.new
+  puts passed_option_list
+  file_option_list.each do |option|
+    if not passed_option_list.include?(option[0])
+      if not non_custom_options.include?(option[0])
+        missing_options.push(option[0])
+      end
+    end
+  end
+  if not missing_options.empty?
+    raise "Your 'custom options' list is missing the following options: #{missing_options}"
+  end
+end
+
+def replace_options(passed_option_list, file_option_list, file_text)
+  file_option_list.each do |option|
+    replace_option = passed_option_list[option]
+    if replace_option.nil?
+      replace_option = passed_option_list[:custom][option]
+    end
+    file_text.gsub!(/\{#{option}\}/, replace_option)
+  end
+  return file_text
 end
 
 # Execute the script
@@ -143,14 +172,12 @@ source = "json-source/{directory}/#{options[:type]}"
 
 options = parse_options(options)
 source_files = find_templates(options, source, model_directories)
-options[:modname] = load_mod_name()
+options[:mod_name] = load_mod_name()
 
-destination = "../src/main/resources/assets/#{options[:modname]}/"
+destination = "../src/main/resources/assets/#{options[:mod_name]}"
 
 all_options = find_options(source_files)
-puts all_options.inspect
+validate_options(options[:custom], all_options, non_custom_options)
 
-puts source_files.inspect
-
-# new_files = copy_templates(options, source, destination)
-# edit_content(options, destination, new_files)
+new_files = copy_templates(options, source_files, destination)
+edit_content(options, destination, new_files)
