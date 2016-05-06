8b6ee4da2da827ab4ec867635002fdba618447a7
Russ Taylor <russt@etsy.com>
2016-05-06 08:34:46 -0700

    Beginning migration to 1.9.

--- src/main/java/us/vombat/lodecraftia/LodecraftiaMod.java
+++ src/main/java/us/vombat/lodecraftia/LodecraftiaMod.java
@@ -18,7 +18,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 @Mod(modid = LodecraftiaMod.MOD_ID, name = LodecraftiaMod.MOD_NAME, version = LodecraftiaMod.VERSION)
 public class LodecraftiaMod {
     public static final String MOD_ID = "lodecraftia";
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.2.1";
     public static final String MOD_NAME = "Lodecraftia";
 
     // World
